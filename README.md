5bb93daa97b83df0ee6256d660d112d0a374d15d
Janne Mattila <janne.mattila@hotmail.fi>
2022-05-20 14:50:48 +0300

    Add endpoint monitoring example

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# endpoint-availability-monitoring-to-app-insights
-Endpoint availability monitoring to app insights
+# Endpoint availability monitoring to app insights
+
+Endpoint availability monitoring to app insights.
+
+## Prepare Azure infra
+
+```bash
+az extension add -n application-insights
+
+workspace_name="log-monitoring"
+ai_name="ai-endpoint"
+location="northeurope"
+resource_group="rg-endpoint-monitoring"
+
+az group create -l $location -n $resource_group -o table
+
+
+workspace_id=$(az monitor log-analytics workspace create -g $resource_group -n $workspace_name --query id -o tsv)
+echo $workspace_id
+
+ai_json=$(az monitor app-insights component create --app $ai_name --location $location --kind web -g $resource_group --workspace $workspace_id -o json)
+ai_connectionstring=$(echo $ai_json | jq -r .connectionString)
+echo $ai_connectionstring
+```
---
+++ src/Endpoint-availability-to-app-insights/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddUserSecrets<Program>()
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddEnvironmentVariables();
+
+var configuration = builder.Build();
+
+var uris = configuration.GetSection("uris").Get<string[]>();
+var frequency = configuration.GetValue<int>("frequency");
+var location = configuration.GetValue<string>("location");
+var connectionstring = configuration.GetValue<string>("connectionstring");
+
+Console.WriteLine($"Starting availability monitoring of {uris.Length} endpoints every {frequency} seconds...");
+
+var client = new HttpClient();
+var telemetryConfiguration = TelemetryConfiguration.CreateDefault();
+telemetryConfiguration.ConnectionString = connectionstring;
+var telemetryClient = new TelemetryClient(telemetryConfiguration);
+
+while (true)
+{
+    Console.WriteLine($"Endpoint availability check {DateTimeOffset.UtcNow}");
+
+    foreach (var uri in uris)
+    {
+        var start = DateTimeOffset.UtcNow;
+        var hostname = new Uri(uri).Host;
+
+        try
+        {
+            var response = await client.GetAsync(uri);
+            var end = DateTimeOffset.UtcNow;
+
+            telemetryClient.TrackAvailability(
+                name: hostname,
+                timeStamp: start,
+                duration: end - start,
+                runLocation: location,
+                success: response.IsSuccessStatusCode,
+                properties: new Dictionary<string, string>(), metrics: new Dictionary<string, double>());
+        }
+        catch (Exception ex)
+        {
+            var end = DateTimeOffset.UtcNow;
+            telemetryClient.TrackAvailability(
+                name: hostname,
+                timeStamp: start,
+                duration: end - start,
+                runLocation: location,
+                success: false,
+                message: ex.Message);
+        }
+    }
+
+    telemetryClient.Flush();
+    await Task.Delay(TimeSpan.FromSeconds(frequency));
+}
